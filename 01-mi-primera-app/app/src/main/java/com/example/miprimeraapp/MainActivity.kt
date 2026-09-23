859603239ead64909dc778f1e73d09ac62470120
Rayan-t-t <rayan.torres@alumfp.udit.es>
2026-09-23 21:23:15 +0200

    gitignore

--- 01-mi-primera-app/app/src/main/java/com/example/miprimeraapp/MainActivity.kt
+++ 01-mi-primera-app/app/src/main/java/com/example/miprimeraapp/MainActivity.kt
@@ -13,35 +13,51 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.miprimeraapp.ui.theme.MiPrimeraAppTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+//class MainActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContent {
+//            MiPrimeraAppTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Rayan muy listo",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    MiPrimeraAppTheme {
+//        Greeting("Android")
+//    }
+//}
+class MainActivity : ComponentActivity(){
+    override  fun onCreate(savedInstanceState : Bundle?){
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            MiPrimeraAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Greeting()
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MiPrimeraAppTheme {
-        Greeting("Android")
-    }
+fun Greeting(){
+    val nombre = "Rayan"
+    val curso = "2do de DAM"
+    val lenguaje ="Kotlin"
+    val mensajeLibre ="Siempre hay espacio para mejorar "
+    Text("Hola, soy $nombre \n $curso \n $lenguaje \n $mensajeLibre")
 }
